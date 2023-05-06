e6af292e7eaae34ec8df6aa882b8af51f8b5aaad
Ivan <aiven715@gmail.com>
2023-05-06 16:17:03 +0600

    jsx parser plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,7 +9,7 @@ module.exports = {
   bracketSpacing: true,
   quoteProps: 'consistent',
   importOrder: ['^~(.*)$', ...generateRelativePathRegexes(10), '^./'],
-  importOrderParserPlugins: ['typescript', 'decorators'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
 }
