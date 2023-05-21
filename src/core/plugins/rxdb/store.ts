6ce00993adba5375ad41d0607acc4c3c46f80ff2
Ivan <aiven715@gmail.com>
2023-05-21 19:22:40 +0600

    Soft deletes

--- src/core/plugins/rxdb/store.ts
+++ src/core/plugins/rxdb/store.ts
@@ -1,19 +1,42 @@
-import { Box } from '../../box'
+import { NotFoundError } from '~/core/errors'
+
 import { LokiJSStore } from '../../stores/lokijs'
 import { DatabaseOptions, Entry, Query } from '../../types'
 
 const LokiIncrementalIndexedDBAdapter = require('lokijs/src/incremental-indexeddb-adapter')
 
-// TODO: perform soft deletes and query based on _deleted
 export class RxDBLokiJSStore extends LokiJSStore {
-  list(collection: string, query?: Query): Box<Entry[]> {
+  list(collection: string, query?: Query) {
     return super.list(collection, {
       ...query,
       filter: {
         ...query?.filter,
-        _deleted: false,
+        [DELETED_KEY]: false,
       },
     })
+  }
+
+  get(collection: string, identifier: string) {
+    return super.get(collection, identifier).then((entry) => {
+      if (entry[DELETED_KEY]) {
+        throw new NotFoundError(identifier)
+      }
+      return entry
+    })
+  }
+
+  set(collection: string, identifier: string, slice: Partial<Entry>) {
+    return this.get(collection, identifier).then(() => {
+      return super.set(collection, identifier, slice)
+    })
+  }
+
+  create(collection: string, document: Entry) {
+    return super.create(collection, { ...document, [DELETED_KEY]: false })
+  }
+
+  remove(collection: string, identifier: string) {
+    return super.set(collection, identifier, { [DELETED_KEY]: true })
   }
 
   static create(options: DatabaseOptions) {
@@ -21,3 +44,5 @@ export class RxDBLokiJSStore extends LokiJSStore {
     return super.create(options, adapter)
   }
 }
+
+const DELETED_KEY = '_deleted'
