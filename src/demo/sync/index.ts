ff61bd76004fadc117bdacfe23ae17aa9bd48bdc
Ivan <aiven715@gmail.com>
2023-06-17 22:11:35 +0300

    Message format utils

--- src/demo/sync/index.ts
+++ src/demo/sync/index.ts
@@ -1,6 +1,15 @@
 import * as Automerge from '@automerge/automerge'
 import { SyncState } from '@automerge/automerge'
 
+import {
+  CREATE_TYPE,
+  UPDATE_TYPE,
+  makeCreateMessage,
+  makeUpdateMessage,
+  parseMessage,
+  parseUpdatePayload,
+} from '~/demo/sync/message'
+
 import { Store } from '../store'
 
 export type Socket = {
@@ -14,70 +23,61 @@ export abstract class Sync {
 
   abstract start(): void
   abstract stop(): void
-  protected abstract getOrCreateSyncState(id: string, socket: Socket): SyncState
+  protected abstract getOrCreateSyncState(id: string, peer: Socket): SyncState
   protected abstract setSyncState(
     id: string,
     nextSyncState: SyncState,
-    socket: Socket
+    peer: Socket
   ): void
 
   sendCreateMessage(binary: Uint8Array, peers = this.peers) {
-    const newBuffer = new ArrayBuffer(1 + binary.byteLength)
-    const newBytes = new Uint8Array(newBuffer)
-    newBytes[0] = 0x00
-    newBytes.set(binary, 1)
+    const message = makeCreateMessage(binary)
     for (const peer of peers) {
-      peer.send(newBytes.buffer)
+      peer.send(message)
     }
   }
 
   sendUpdateMessage(id: string, binary: Uint8Array) {
     for (const peer of this.peers) {
       const syncState = this.getOrCreateSyncState(id, peer)
+      const document = Automerge.load(binary)
       const [nextSyncState, nextSyncMessage] = Automerge.generateSyncMessage(
-        Automerge.load(binary),
+        document,
         syncState
       )
       this.setSyncState(id, nextSyncState, peer)
       if (nextSyncMessage) {
-        const binaryId = new TextEncoder().encode(id)
-        const newBuffer = new ArrayBuffer(1 + 32 + nextSyncMessage.byteLength)
-        const newBytes = new Uint8Array(newBuffer)
-        newBytes[0] = 0x01
-        newBytes.set(binaryId, 1)
-        newBytes.set(nextSyncMessage, 1 + 32)
-        peer.send(newBytes.buffer)
+        const message = makeUpdateMessage(id, nextSyncMessage)
+        peer.send(message)
       }
     }
   }
 
   // TODO: lock document
   async receiveMessage(message: ArrayBuffer, peer: Socket) {
-    const binary = new Uint8Array(message)
-    const type = binary[0]
-    const payload = binary.subarray(1)
+    const { type, payload } = parseMessage(message)
     switch (type) {
-      case 0x00: {
+      case CREATE_TYPE: {
         await this.store.set(payload)
         const peers = this.peers.filter((s) => s !== peer)
         this.sendCreateMessage(payload, peers)
         return
       }
-      case 0x01: {
-        const binaryId = payload.subarray(0, 32)
-        const syncMessage = payload.subarray(32)
-        const id = new TextDecoder().decode(binaryId)
+      case UPDATE_TYPE: {
+        const { id, syncMessage } = parseUpdatePayload(payload)
         // TODO: what if we'll get update of a document which does not exist?
         //       can we get it?
-        const [document, syncState] = Automerge.receiveSyncMessage(
-          Automerge.load(await this.store.get(id)),
+        const binary = await this.store.get(id)
+        const document = Automerge.load(binary)
+        const [nextDocument, nextSyncState] = Automerge.receiveSyncMessage(
+          document,
           this.getOrCreateSyncState(id, peer),
           syncMessage
         )
-        const binary = Automerge.save(document)
-        await this.store.set(binary)
-        this.setSyncState(id, syncState, peer)
-        this.sendUpdateMessage(id, binary)
+        const nextBinary = Automerge.save(nextDocument)
+        await this.store.set(nextBinary)
+        this.setSyncState(id, nextSyncState, peer)
+        this.sendUpdateMessage(id, nextBinary)
         return
       }
       default:
--- src/demo/sync/message.ts
+++ src/demo/sync/message.ts
@@ -1,3 +1,36 @@
-export const makeCreateMessage = () => {}
+import { SyncMessage } from '@automerge/automerge'
 
-export const makeUpdateMessage = () => {}
+export const CREATE_TYPE = 0x00
+export const UPDATE_TYPE = 0x01
+
+export const parseMessage = (message: ArrayBuffer) => {
+  const binary = new Uint8Array(message)
+  const type = binary[0]
+  const payload = binary.subarray(1)
+  return { type, payload }
+}
+
+export const parseUpdatePayload = (payload: Uint8Array) => {
+  const binaryId = payload.subarray(0, 32)
+  const syncMessage = payload.subarray(32)
+  const id = new TextDecoder().decode(binaryId)
+  return { id, syncMessage }
+}
+
+export const makeCreateMessage = (binary: Uint8Array) => {
+  const newBuffer = new ArrayBuffer(1 + binary.byteLength)
+  const newBytes = new Uint8Array(newBuffer)
+  newBytes[0] = CREATE_TYPE
+  newBytes.set(binary, 1)
+  return newBytes.buffer
+}
+
+export const makeUpdateMessage = (id: string, syncMessage: SyncMessage) => {
+  const binaryId = new TextEncoder().encode(id)
+  const newBuffer = new ArrayBuffer(1 + 32 + syncMessage.byteLength)
+  const newBytes = new Uint8Array(newBuffer)
+  newBytes[0] = UPDATE_TYPE
+  newBytes.set(binaryId, 1)
+  newBytes.set(syncMessage, 1 + 32)
+  return newBytes.buffer
+}
